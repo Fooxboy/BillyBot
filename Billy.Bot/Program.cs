b4f8d25af85c261d79757097b9069851a98660e8
Fooxboy <Fooxboy@yandex.ru>
2017-11-20 17:41:08 +0300

    Добавьте файлы проекта.

---
+++ Billy.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Billy.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
