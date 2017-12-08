59be875971edbc3c041dc1719110dbdb053fbca3
Fooxboy <Fooxboy@yandex.ru>
2017-12-08 08:09:13 +0300

    0.4.7

---
+++ Billy.Commands/Lot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Billy.Models;
+
+namespace Billy.Commands
+{
+    public class Lot: ICommand
+    {
+        Data.Commands.IDataCommand data = new Data.Commands.Lot();
+        public string Name => data.Name;
+        public string Help => data.Help;
+        public string FullHelp => data.FullHelp;
+        public List<Enums.Billy.Donate> Donate => Samples.AccessDonate.All;
+
+        public void Execute(Message message, string[] arguments)
+        {
+            string result = "Неизвестная ошибка.";
+            var vk = API.Data.GetVk();
+            var vkMessage = vk.Messages.GetById(message.MessageId);
+            int count = vkMessage.ForwardedMessages.Count;
+            if(count < 2)
+            {
+                result = Data.Commands.Lot.NoForward;
+            }else
+            {
+                var rand = new Random();
+                int index = rand.Next(0, count - 1);
+                var forwardMessage = vkMessage.ForwardedMessages[index];
+                var userId = forwardMessage.UserId.Value;
+                var user = vk.Users.Get(userId);
+                var firstName = user.FirstName;
+                var lastName = user.LastName;
+                result = Data.Commands.Lot.Ready(firstName, lastName);
+            }
+
+            API.Message.Send(new Models.Params.MessageSendParams
+            {
+                PeerId = message.PeerId,
+                Message = result,
+                From = message.From
+            });
+        }
+    }
+}
---
+++ Billy.Data/Commands/Lot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Billy.Data.Commands
+{
+    public class Lot : IDataCommand
+    {
+        public string Name => "Жребий";
+        public string Help => "Выберает одного из нескольких.";
+        public string FullHelp => "Выберает одного пользователя из нескольких." +
+            "\nИСПОЛЬЗОВАНИЕ:" +
+            "\nБилли жребий (2 и более пересланных сообщений)";
+        public static string Ready(string name, string name2)
+        {
+            string text = $"И я выбираю {name} {name2}!";
+            return text;
+        }
+        public static string NoForward = "Количество пересланных сообщений должно быть 2 и выше!";
+    }
+}
