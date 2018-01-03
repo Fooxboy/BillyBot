c42e06ce6d3654ee904af392b10ce83c1bf2dc9c
Fooxboy <Fooxboy@yandex.ru>
2018-01-03 09:10:26 +0300

    0.4.14

---
+++ Billy.Mafia/Roles/Самоубийца.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Billy.Mafia.Roles
+{
+    public class Самоубийца : IRole
+    {
+        public string Name => "Самоубийца";
+        public string Caption => "Одиночка.. Единственная его цель - чтобы его убили при голосовании. Он за это получит бонус :)";
+        public string Message => "Хах! Вы -  самоубийца. Вы должны любой ценой умереть при голосовании! За это ты получишь бонус)0)0";
+        public int Id => 9;
+        public RoleEnum type => RoleEnum.Одиночки;
+    }
+}
---
+++ Billy.Mafia/Roles/Сыщик.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Billy.Mafia.Roles
+{
+    public class Сыщик : IRole
+    {
+        public string Name => "Сыщик";
+        public string Caption => "Местный наёмный сыщик. Может найти кого-угодно, но не факт, что он будет точен.";
+        public string Message => "Воу! Воу! Вы - местный сыщик. Найти для вас бандитов - как раз плюнуть! Но результат будет не очень точен :)";
+        public int id => 10;
+        public RoleEnum type => RoleEnum.Жители;
+    }
+}
